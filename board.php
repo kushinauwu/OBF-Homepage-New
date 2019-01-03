49d3fdc4e28f7cb1e2f0a23cb5fdf97fa2a22ecc
Deepashree Deshmukh <18piscean@gmail.com>
2019-01-03 23:34:12 +0530

    updated board

--- board.php
+++ board.php
@@ -80,9 +80,11 @@ Template Name:board
                                 <div class="col-sm-4 col-xs-4">
                                     <li class="member-details">
                                             <!--<img src="member.jpg">-->
-                                        <?php the_post_thumbnail(); ?>
+                                        <?php $member_url = get_post_meta( get_the_ID(), 'external_url', true ); ?>
+                                        <a href="<?php echo $member_url; ?>" target="_blank"><?php the_post_thumbnail(); ?></a>
                                             <div class="member-name">
-                                                <?php the_title(); ?>
+                                                <?php $member_url = get_post_meta( get_the_ID(), 'external_url', true ); ?>
+                                                <a href="<?php echo $member_url; ?>" target="_blank"><?php the_title(); ?></a>
                                             </div>
                                             <div class="member-position">
                                                 <?php the_field('member_position'); ?>
@@ -103,42 +105,61 @@ Template Name:board
                
                <section class="join-board">
                    <div class="container-fluid">
-                    <h1>Joining the board</h1>
+                       <?php $join_args = array(
+                                            'name' => 'joining-the-board',
+                                            'post-type' => 'post',
+                                            'post-status' => 'publish',
+                                                ); ?>
+                       <?php $join_query = new WP_Query( $join_args ); ?>
+                       <?php if ( $join_query->have_posts() ) : while ( $join_query->have_posts() ) : $join_query->the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
                     <div class="row">
                         <div class="joining-text">
                         <div class="col-md-12">
-                            <p>Nominations for the Board of Directors may be made by the general membership at any time throughout the year by email to any Board Member</p>
-                            <p>The Board tries on an ongoing basis to create opportunities for members of the community to serve, while also ensuring stability of the Board, through expansion of the Board, by replacing a Board member whose term expires,  or by volunteers from the community interested in serving</p>
+                            <p><?php the_content(); ?></p>
                             </div>
                     </div>
                     </div>
+                       <?php endwhile; else : ?>
+                            <p><?php __('No post found'); ?></p>
+                            <?php endif;
+                            wp_reset_postdata(); ?>
+                       
                        <div class="row expectations-heading">
+                           <?php $expectations_args = array(
+                                            'name' => 'board-expectations',
+                                            'post-type' => 'post',
+                                            'post-status' => 'publish',
+                                                ); ?>
+                       <?php $expectations_query = new WP_Query( $expectations_args ); ?>
+                       <?php if ( $expectations_query->have_posts() ) : while ( $expectations_query->have_posts() ) : $expectations_query->the_post(); ?>
                            <div class="col-xs-12">
-                               <h2>Board Expectations</h2>
+                               <h2><?php the_title(); ?></h2>
                            </div>
                         
                         <div class="expectations-text-wrapper">
-                            <ul class="expectations-list list-unstyled row">
+                            <!--<ul class="expectations-list list-unstyled row">-->
                                 <div class="col-xs-12">
-                                    <li class="expectations">
-                                        <p>Participate in the (informal) <strong>regular meetings</strong> of Board members. Meetings are currently about 1 hour, <strong>once a month</strong>, held by Google Hangout. These meetings are for communicating and discussing <strong>current OBF operations and issues</strong>, as well as brainstorming strategic <strong>directions and initiatives</strong>.</p>
-                                    </li>
-                                    <li class="expectations">
-                                        <p>Participate in <strong>public Board meetings</strong>, including <strong>voting in elections</strong> and on matters brought before the OBF Board for approval. Public Board meetings take place <strong>1-2 times a year </strong>via conference call, are about 1 hour in duration, and are usually scheduled in place of a Board hangout.</p>
+                                    
+                                    <!--<li class="expectations">-->
+                                        <?php the_content(); ?>
 
-                                    </li>
-                                    <li class="expectations">
-                                        <p>Help <strong>promote the causes and objectives</strong> of the OBF, as set and approved by the Board, such as <strong>lobbying funders, employers, and other decision makers</strong> in your network about these objectives and ways to achieve them, as well as promoting the causes of the OBF at community gatherings you attend.</p>
-                                    </li>
-                                    <li class="expectations">
-                                        <p>Contribute to identifying strategies, opportunities, and mechanisms to better <strong>sustain the OBF</strong>, to <strong>grow its member communities</strong>, and to increase its overall <strong>diversity</strong>, including demographic diversity.</p>
-                                    </li>
+                                    <!--</li>-->
+                                    
                                 </div>
-                            </ul>
+                            <!--</ul>-->
                         </div>
+                           <?php endwhile; else : ?>
+                            <p><?php __('No post found'); ?></p>
+                            <?php endif;
+                            wp_reset_postdata(); ?>
                        </div>
                 </div>   
-               </section>             
+               </section>
+               
+ 
+                        
+               
            </div>
 
 <?php get_footer(); ?>
